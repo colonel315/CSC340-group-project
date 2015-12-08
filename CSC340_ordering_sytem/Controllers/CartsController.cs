3e7564fa39de34bbe624a4f59ff8e4787e7387ce
colonel315 <Fred_Burr@mymail.eku.edu>
2015-12-08 17:17:57 -0500

    wokring on cart module

--- CSC340_ordering_sytem/Controllers/CartsController.cs
+++ CSC340_ordering_sytem/Controllers/CartsController.cs
@@ -1,9 +1,9 @@
 ﻿using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web.Mvc;
 using CSC340_ordering_sytem.DAL;
 using CSC340_ordering_sytem.Models;
+using Microsoft.AspNet.Identity;
 
 namespace CSC340_ordering_sytem.Controllers
 {
@@ -12,105 +12,117 @@ namespace CSC340_ordering_sytem.Controllers
         private readonly OrderingSystemDbContext _db = new OrderingSystemDbContext();
 
         // GET: Carts
-        public ActionResult Index()
+        public ActionResult ShowCart()
         {
-            return View(_db.Carts.ToList());
+            var userId = int.Parse(User.Identity.GetUserId());
+            
+            var customer = _db.Users.OfType<Customer>().Where(c => c.Id == userId).Include("Cart").Include("CartItem");
+
+            return View(customer);
         }
+
+
+
+        // GET: Carts
+//        public ActionResult Index()
+//        {
+//            return View(_db.Carts.ToList());
+//        }
 
         // GET: Carts/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Cart cart = _db.Carts.Find(id);
-            if (cart == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cart);
-        }
-
-        // GET: Carts/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Carts/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id")] Cart cart)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Carts.Add(cart);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(cart);
-        }
-
-        // GET: Carts/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Cart cart = _db.Carts.Find(id);
-            if (cart == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cart);
-        }
-
-        // POST: Carts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id")] Cart cart)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Entry(cart).State = EntityState.Modified;
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(cart);
-        }
-
-        // GET: Carts/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Cart cart = _db.Carts.Find(id);
-            if (cart == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cart);
-        }
-
-        // POST: Carts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Cart cart = _db.Carts.Find(id);
-            _db.Carts.Remove(cart);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+//        public ActionResult Details(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            Cart cart = _db.Carts.Find(id);
+//            if (cart == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(cart);
+//        }
+//
+//        // GET: Carts/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
+//
+//        // POST: Carts/Create
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create([Bind(Include = "Id")] Cart cart)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                _db.Carts.Add(cart);
+//                _db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//
+//            return View(cart);
+//        }
+//
+//        // GET: Carts/Edit/5
+//        public ActionResult Edit(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            Cart cart = _db.Carts.Find(id);
+//            if (cart == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(cart);
+//        }
+//
+//        // POST: Carts/Edit/5
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit([Bind(Include = "Id")] Cart cart)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                _db.Entry(cart).State = EntityState.Modified;
+//                _db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            return View(cart);
+//        }
+//
+//        // GET: Carts/Delete/5
+//        public ActionResult Delete(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            Cart cart = _db.Carts.Find(id);
+//            if (cart == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(cart);
+//        }
+//
+//        // POST: Carts/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteConfirmed(int id)
+//        {
+//            Cart cart = _db.Carts.Find(id);
+//            _db.Carts.Remove(cart);
+//            _db.SaveChanges();
+//            return RedirectToAction("Index");
+//        }
 
         protected override void Dispose(bool disposing)
         {
